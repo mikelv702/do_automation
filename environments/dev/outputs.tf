1c87db6c6872afe6812ff65970e159dade51e6f6
Michael H <michael@rubberduck-labs.com>
2024-06-24 17:30:39 -0600

    Fix the format

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -2,9 +2,9 @@ output "droplet_get_ids" {
   value = module.jumphosts.droplet_ids
 }
 output "droplet_get_ips" {
-  value =  module.jumphosts.droplet_ips
+  value = module.jumphosts.droplet_ips
 }
 
 output "droplet_get_urns" {
-  value =  module.jumphosts.droplet_urns
+  value = module.jumphosts.droplet_urns
 }
