69f1d169aa2d8e8b7a477cf055d957b3c46b8321
Michael H <michael@rubberduck-labs.com>
2024-06-24 09:44:01 -0600

    Adding firewalls

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,10 +2,18 @@
 module "jumphosts" {
   source = "../../modules/digital_ocean_droplet"
 
-  droplet_count = 1
+  droplet_count = 2
   group_name    = "jumphost"
   prefix = "dev"
   domain_name = var.domain_name
   droplet_image_name = "ubuntu-22-04-x64"
 }
 
+module "fw_jumphosts" {
+  source = "../../modules/digital_ocean_firewall"
+
+  group_name    = "jumphost"
+  prefix = "dev"
+  droplet_ids = module.jumphosts.droplet_ids[*]
+  allow_ssh_inbound_subnet = "192.168.1.0/32"
+}
---
+++ environments/dev/outputs.tf
@@ -0,0 +1,6 @@
+output "droplet_get_ids" {
+  value = module.jumphosts.droplet_ids
+}
+output "droplet_get_ips" {
+  value =  module.jumphosts.droplet_ips
+}
